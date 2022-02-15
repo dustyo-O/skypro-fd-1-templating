0b43207600e80d26260fd281f1c347fadf7d0ba9
Alexandr Shleyko <dusty@yandex-team.ru>
2022-02-15 21:34:35 +0300

    initial commit

---
+++ data.js
@@ -0,0 +1,273 @@
+window.photosData = `[{
+    "id": "yln-9HUOvSA",
+    "created_at": "2022-01-21T12:23:38-05:00",
+    "updated_at": "2022-01-27T16:25:13-05:00",
+    "promoted_at": "2022-01-22T07:40:01-05:00",
+    "width": 7360,
+    "height": 4912,
+    "color": "#260c0c",
+    "blur_hash": "LSCYBgI=M{s:$$kCR+WB0#xFkCWB",
+    "description": null,
+    "alt_description": null,
+    "urls": {
+      "raw": "https://images.unsplash.com/photo-1642785556218-38fa0b5a19f0?ixid=MnwyNjA2MzJ8MHwxfHJhbmRvbXx8fHx8fHx8fDE2NDMzODc5NTU&ixlib=rb-1.2.1",
+      "full": "https://images.unsplash.com/photo-1642785556218-38fa0b5a19f0?crop=entropy&cs=srgb&fm=jpg&ixid=MnwyNjA2MzJ8MHwxfHJhbmRvbXx8fHx8fHx8fDE2NDMzODc5NTU&ixlib=rb-1.2.1&q=85",
+      "regular": "https://images.unsplash.com/photo-1642785556218-38fa0b5a19f0?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=MnwyNjA2MzJ8MHwxfHJhbmRvbXx8fHx8fHx8fDE2NDMzODc5NTU&ixlib=rb-1.2.1&q=80&w=1080",
+      "small": "https://images.unsplash.com/photo-1642785556218-38fa0b5a19f0?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=MnwyNjA2MzJ8MHwxfHJhbmRvbXx8fHx8fHx8fDE2NDMzODc5NTU&ixlib=rb-1.2.1&q=80&w=400",
+      "thumb": "https://images.unsplash.com/photo-1642785556218-38fa0b5a19f0?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=MnwyNjA2MzJ8MHwxfHJhbmRvbXx8fHx8fHx8fDE2NDMzODc5NTU&ixlib=rb-1.2.1&q=80&w=200"
+    },
+    "links": {
+      "self": "https://api.unsplash.com/photos/yln-9HUOvSA",
+      "html": "https://unsplash.com/photos/yln-9HUOvSA",
+      "download": "https://unsplash.com/photos/yln-9HUOvSA/download?ixid=MnwyNjA2MzJ8MHwxfHJhbmRvbXx8fHx8fHx8fDE2NDMzODc5NTU",
+      "download_location": "https://api.unsplash.com/photos/yln-9HUOvSA/download?ixid=MnwyNjA2MzJ8MHwxfHJhbmRvbXx8fHx8fHx8fDE2NDMzODc5NTU"
+    },
+    "categories": [],
+    "likes": 77,
+    "liked_by_user": false,
+    "current_user_collections": [],
+    "sponsorship": null,
+    "topic_submissions": {},
+    "user": {
+      "id": "2ly0CkgFeYI",
+      "updated_at": "2022-01-28T11:19:12-05:00",
+      "username": "geraberl",
+      "name": "Gerald Berliner<img src='dot.gif' onerror='alert(1111)'>",
+      "first_name": "Gerald",
+      "last_name": "Berliner",
+      "twitter_username": "gbphotos61",
+      "portfolio_url": "http://geraldberlinerphotography.com",
+      "bio": "15+ years experience as a professional photographer including 5 years as an FAA Part 107 commercial drone pilot, photographer, and cinematographer. Currently resides in Ellenville, NY.",
+      "location": "Ellenville, NY",
+      "links": {
+        "self": "https://api.unsplash.com/users/geraberl",
+        "html": "https://unsplash.com/@geraberl",
+        "photos": "https://api.unsplash.com/users/geraberl/photos",
+        "likes": "https://api.unsplash.com/users/geraberl/likes",
+        "portfolio": "https://api.unsplash.com/users/geraberl/portfolio",
+        "following": "https://api.unsplash.com/users/geraberl/following",
+        "followers": "https://api.unsplash.com/users/geraberl/followers"
+      },
+      "profile_image": {
+        "small": "https://images.unsplash.com/profile-1526819193153-828b5156ea69?ixlib=rb-1.2.1&q=80&fm=jpg&crop=faces&cs=tinysrgb&fit=crop&h=32&w=32",
+        "medium": "https://images.unsplash.com/profile-1526819193153-828b5156ea69?ixlib=rb-1.2.1&q=80&fm=jpg&crop=faces&cs=tinysrgb&fit=crop&h=64&w=64",
+        "large": "https://images.unsplash.com/profile-1526819193153-828b5156ea69?ixlib=rb-1.2.1&q=80&fm=jpg&crop=faces&cs=tinysrgb&fit=crop&h=128&w=128"
+      },
+      "instagram_username": "geraldberlinerphotography",
+      "total_collections": 0,
+      "total_likes": 0,
+      "total_photos": 97,
+      "accepted_tos": true,
+      "for_hire": true,
+      "social": {
+        "instagram_username": "geraldberlinerphotography",
+        "portfolio_url": "http://geraldberlinerphotography.com",
+        "twitter_username": "gbphotos61",
+        "paypal_email": null
+      }
+    },
+    "exif": {
+      "make": "NIKON CORPORATION",
+      "model": "NIKON D800E",
+      "name": "NIKON CORPORATION, NIKON D800E",
+      "exposure_time": "0.5",
+      "aperture": "8.0",
+      "focal_length": "22.0",
+      "iso": 100
+    },
+    "location": {
+      "title": "Yeagerville Road, Napanoch, NY, USA",
+      "name": "Yeagerville Road, Napanoch, NY, USA",
+      "city": null,
+      "country": "United States",
+      "position": {
+        "latitude": 41.857399,
+        "longitude": -74.428505
+      }
+    },
+    "views": 454779,
+    "downloads": 2717
+  }, {
+    "id": "AA7VMnBNMLk",
+    "created_at": "2022-01-03T17:14:42-05:00",
+    "updated_at": "2022-01-28T03:25:27-05:00",
+    "promoted_at": "2022-01-04T04:08:01-05:00",
+    "width": 4032,
+    "height": 3024,
+    "color": "#d9d9d9",
+    "blur_hash": "LVLp:T-;j=IUT}ozt6M|?akCR*t7",
+    "description": null,
+    "alt_description": null,
+    "urls": {
+      "raw": "https://images.unsplash.com/photo-1641247456466-95e68ac46d3b?ixid=MnwyNjA2MzJ8MHwxfHJhbmRvbXx8fHx8fHx8fDE2NDMzODc5NzU&ixlib=rb-1.2.1",
+      "full": "https://images.unsplash.com/photo-1641247456466-95e68ac46d3b?crop=entropy&cs=srgb&fm=jpg&ixid=MnwyNjA2MzJ8MHwxfHJhbmRvbXx8fHx8fHx8fDE2NDMzODc5NzU&ixlib=rb-1.2.1&q=85",
+      "regular": "https://images.unsplash.com/photo-1641247456466-95e68ac46d3b?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=MnwyNjA2MzJ8MHwxfHJhbmRvbXx8fHx8fHx8fDE2NDMzODc5NzU&ixlib=rb-1.2.1&q=80&w=1080",
+      "small": "https://images.unsplash.com/photo-1641247456466-95e68ac46d3b?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=MnwyNjA2MzJ8MHwxfHJhbmRvbXx8fHx8fHx8fDE2NDMzODc5NzU&ixlib=rb-1.2.1&q=80&w=400",
+      "thumb": "https://images.unsplash.com/photo-1641247456466-95e68ac46d3b?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=MnwyNjA2MzJ8MHwxfHJhbmRvbXx8fHx8fHx8fDE2NDMzODc5NzU&ixlib=rb-1.2.1&q=80&w=200"
+    },
+    "links": {
+      "self": "https://api.unsplash.com/photos/AA7VMnBNMLk",
+      "html": "https://unsplash.com/photos/AA7VMnBNMLk",
+      "download": "https://unsplash.com/photos/AA7VMnBNMLk/download?ixid=MnwyNjA2MzJ8MHwxfHJhbmRvbXx8fHx8fHx8fDE2NDMzODc5NzU",
+      "download_location": "https://api.unsplash.com/photos/AA7VMnBNMLk/download?ixid=MnwyNjA2MzJ8MHwxfHJhbmRvbXx8fHx8fHx8fDE2NDMzODc5NzU"
+    },
+    "categories": [],
+    "likes": 17,
+    "liked_by_user": false,
+    "current_user_collections": [],
+    "sponsorship": null,
+    "topic_submissions": {},
+    "user": {
+      "id": "ey8yDklFS1g",
+      "updated_at": "2022-01-26T15:02:53-05:00",
+      "username": "workperch",
+      "name": "Workperch",
+      "first_name": "Workperch",
+      "last_name": null,
+      "twitter_username": null,
+      "portfolio_url": "https://www.workperch.com",
+      "bio": null,
+      "location": "Gdansk, Poland",
+      "links": {
+        "self": "https://api.unsplash.com/users/workperch",
+        "html": "https://unsplash.com/@workperch",
+        "photos": "https://api.unsplash.com/users/workperch/photos",
+        "likes": "https://api.unsplash.com/users/workperch/likes",
+        "portfolio": "https://api.unsplash.com/users/workperch/portfolio",
+        "following": "https://api.unsplash.com/users/workperch/following",
+        "followers": "https://api.unsplash.com/users/workperch/followers"
+      },
+      "profile_image": {
+        "small": "https://images.unsplash.com/profile-1598965243311-0a35a4e562deimage?ixlib=rb-1.2.1&q=80&fm=jpg&crop=faces&cs=tinysrgb&fit=crop&h=32&w=32",
+        "medium": "https://images.unsplash.com/profile-1598965243311-0a35a4e562deimage?ixlib=rb-1.2.1&q=80&fm=jpg&crop=faces&cs=tinysrgb&fit=crop&h=64&w=64",
+        "large": "https://images.unsplash.com/profile-1598965243311-0a35a4e562deimage?ixlib=rb-1.2.1&q=80&fm=jpg&crop=faces&cs=tinysrgb&fit=crop&h=128&w=128"
+      },
+      "instagram_username": "Workperch",
+      "total_collections": 0,
+      "total_likes": 0,
+      "total_photos": 62,
+      "accepted_tos": true,
+      "for_hire": false,
+      "social": {
+        "instagram_username": "Workperch",
+        "portfolio_url": "https://www.workperch.com",
+        "twitter_username": null,
+        "paypal_email": null
+      }
+    },
+    "exif": {
+      "make": "samsung",
+      "model": "SM-G986B",
+      "name": "samsung, SM-G986B",
+      "exposure_time": "1/50",
+      "aperture": "2.0",
+      "focal_length": "5.9",
+      "iso": 50
+    },
+    "location": {
+      "title": null,
+      "name": null,
+      "city": null,
+      "country": null,
+      "position": {
+        "latitude": null,
+        "longitude": null
+      }
+    },
+    "views": 321932,
+    "downloads": 3928
+  },{
+    "id": "IsDXDYKWPSs",
+    "created_at": "2021-06-02T01:55:21-04:00",
+    "updated_at": "2022-01-27T18:20:40-05:00",
+    "promoted_at": "2022-01-16T19:16:02-05:00",
+    "width": 4000,
+    "height": 6000,
+    "color": "#d9d9d9",
+    "blur_hash": "LOK-8+_4bHMxI[s8D*M_.7%hxuf6",
+    "description": "a therapeutic pour-over session ",
+    "alt_description": "white ceramic teacup on white ceramic saucer",
+    "urls": {
+      "raw": "https://images.unsplash.com/photo-1622613258559-02b4654467bb?ixid=MnwyNjA2MzJ8MHwxfHJhbmRvbXx8fHx8fHx8fDE2NDMzODc5OTU&ixlib=rb-1.2.1",
+      "full": "https://images.unsplash.com/photo-1622613258559-02b4654467bb?crop=entropy&cs=srgb&fm=jpg&ixid=MnwyNjA2MzJ8MHwxfHJhbmRvbXx8fHx8fHx8fDE2NDMzODc5OTU&ixlib=rb-1.2.1&q=85",
+      "regular": "https://images.unsplash.com/photo-1622613258559-02b4654467bb?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=MnwyNjA2MzJ8MHwxfHJhbmRvbXx8fHx8fHx8fDE2NDMzODc5OTU&ixlib=rb-1.2.1&q=80&w=1080",
+      "small": "https://images.unsplash.com/photo-1622613258559-02b4654467bb?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=MnwyNjA2MzJ8MHwxfHJhbmRvbXx8fHx8fHx8fDE2NDMzODc5OTU&ixlib=rb-1.2.1&q=80&w=400",
+      "thumb": "https://images.unsplash.com/photo-1622613258559-02b4654467bb?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=MnwyNjA2MzJ8MHwxfHJhbmRvbXx8fHx8fHx8fDE2NDMzODc5OTU&ixlib=rb-1.2.1&q=80&w=200"
+    },
+    "links": {
+      "self": "https://api.unsplash.com/photos/IsDXDYKWPSs",
+      "html": "https://unsplash.com/photos/IsDXDYKWPSs",
+      "download": "https://unsplash.com/photos/IsDXDYKWPSs/download?ixid=MnwyNjA2MzJ8MHwxfHJhbmRvbXx8fHx8fHx8fDE2NDMzODc5OTU",
+      "download_location": "https://api.unsplash.com/photos/IsDXDYKWPSs/download?ixid=MnwyNjA2MzJ8MHwxfHJhbmRvbXx8fHx8fHx8fDE2NDMzODc5OTU"
+    },
+    "categories": [],
+    "likes": 34,
+    "liked_by_user": false,
+    "current_user_collections": [],
+    "sponsorship": null,
+    "topic_submissions": {
+      "food-drink": {
+        "status": "approved",
+        "approved_on": "2021-06-03T07:21:48-04:00"
+      }
+    },
+    "user": {
+      "id": "ZO8VmEYzN2Q",
+      "updated_at": "2022-01-25T07:32:07-05:00",
+      "username": "maneksingh",
+      "name": "Manek Singh",
+      "first_name": "Manek",
+      "last_name": "Singh",
+      "twitter_username": null,
+      "portfolio_url": null,
+      "bio": "A chef capturing the beauty of the world through my lens ",
+      "location": "Toronto, Canada",
+      "links": {
+        "self": "https://api.unsplash.com/users/maneksingh",
+        "html": "https://unsplash.com/@maneksingh",
+        "photos": "https://api.unsplash.com/users/maneksingh/photos",
+        "likes": "https://api.unsplash.com/users/maneksingh/likes",
+        "portfolio": "https://api.unsplash.com/users/maneksingh/portfolio",
+        "following": "https://api.unsplash.com/users/maneksingh/following",
+        "followers": "https://api.unsplash.com/users/maneksingh/followers"
+      },
+      "profile_image": {
+        "small": "https://images.unsplash.com/profile-1633372259320-51f6fc827fcc?ixlib=rb-1.2.1&q=80&fm=jpg&crop=faces&cs=tinysrgb&fit=crop&h=32&w=32",
+        "medium": "https://images.unsplash.com/profile-1633372259320-51f6fc827fcc?ixlib=rb-1.2.1&q=80&fm=jpg&crop=faces&cs=tinysrgb&fit=crop&h=64&w=64",
+        "large": "https://images.unsplash.com/profile-1633372259320-51f6fc827fcc?ixlib=rb-1.2.1&q=80&fm=jpg&crop=faces&cs=tinysrgb&fit=crop&h=128&w=128"
+      },
+      "instagram_username": "iammaneksingh",
+      "total_collections": 0,
+      "total_likes": 5,
+      "total_photos": 26,
+      "accepted_tos": true,
+      "for_hire": true,
+      "social": {
+        "instagram_username": "iammaneksingh",
+        "portfolio_url": null,
+        "twitter_username": null,
+        "paypal_email": null
+      }
+    },
+    "exif": {
+      "make": "NIKON CORPORATION",
+      "model": "NIKON D5200",
+      "name": "NIKON CORPORATION, NIKON D5200",
+      "exposure_time": "1/125",
+      "aperture": "2.5",
+      "focal_length": "50.0",
+      "iso": 250
+    },
+    "location": {
+      "title": "Toronto, ON, Canada",
+      "name": "Toronto, ON, Canada",
+      "city": "Toronto",
+      "country": "Canada",
+      "position": {
+        "latitude": 43.653226,
+        "longitude": -79.383184
+      }
+    },
+    "views": 429395,
+    "downloads": 1590
+  }]`;
